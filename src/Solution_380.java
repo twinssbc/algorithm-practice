7314cb09da1b2197212264282a961f10cb397d6c
Bocong Sui <bocong@hotmail.com>
2023-02-04 23:40:26 +0800

    add solution 380

---
+++ src/Solution_380.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution_380 {
+    class RandomizedSet {
+        Map<Integer, Integer> elementIndexMap = new HashMap<>();
+        List<Integer> elementList = new ArrayList<>();
+        int elementCount = 0;
+        public RandomizedSet() {
+
+        }
+
+        public boolean insert(int val) {
+            if(elementIndexMap.containsKey(val)) {
+                return false;
+            }
+            elementList.add(elementCount, val);
+            elementIndexMap.put(val, elementCount);
+            elementCount++;
+            return true;
+        }
+
+        public boolean remove(int val) {
+            if(elementIndexMap.containsKey(val)) {
+                int index = elementIndexMap.get(val);
+                if(index != elementCount - 1) {
+                    int swapElement = elementList.get(elementCount-1);
+                    elementList.set(index, swapElement);
+                    elementIndexMap.put(swapElement, index);
+                }
+                elementIndexMap.remove(val);
+                elementCount--;
+                return true;
+            }
+            return false;
+        }
+
+        public int getRandom() {
+            int index = (int)(elementCount * Math.random());
+            return elementList.get(index);
+        }
+    }
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
+}
---
+++ src/Solution_528.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class Solution_528 {
+    int[] freq;
+    private Random rand = new Random();
+    private int sum;
+    public Solution_528(int[] w) {
+        freq = new int[w.length];
+        freq[0] = w[0];
+        sum = w[0];
+        for(int i = 1; i < w.length; i++) {
+            freq[i] = freq[i-1] + w[i];
+            sum+=w[i];
+        }
+    }
+
+    public int pickIndex() {
+        int randValue = rand.nextInt(sum) + 1;
+        int l = 0;
+        int r = freq.length - 1;
+        while(l < r) {
+            int mid = (l+r)/2;
+            if(randValue > freq[mid]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        if(freq[l] >= randValue) {
+            return l;
+        } else {
+            return l + 1;
+        }
+    }
+}
