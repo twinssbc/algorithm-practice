e41af1516c068f5d24a6c72d73cdec4d8c68e67f
Bocong Sui <bocong@hotmail.com>
2023-02-04 15:35:29 +0800

    add solution 710

---
+++ src/Solution_710.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Solution_710 {
+    private Map<Integer, Integer> remap = new HashMap<>();
+    private Random random;
+    private int whitelistNumber;
+    public Solution_710(int n, int[] blacklist) {
+        Set<Integer> whiteList = new HashSet<>();
+        whitelistNumber = n - blacklist.length;
+        random = new Random();
+        for(int i = whitelistNumber; i < n; i++) {
+            whiteList.add(i);
+        }
+        for(int i = 0; i < blacklist.length; i++) {
+            whiteList.remove(blacklist[i]);
+        }
+
+        Iterator<Integer> itr = whiteList.iterator();
+        for(int i = 0; i < blacklist.length; i++) {
+            if(blacklist[i] < whitelistNumber) {
+                remap.put(blacklist[i], itr.next());
+            }
+        }
+    }
+
+    public int pick() {
+        int rand = random.nextInt(whitelistNumber);
+        if(remap.containsKey(rand)) {
+            return remap.get(rand);
+        }
+        return rand;
+    }
+}
