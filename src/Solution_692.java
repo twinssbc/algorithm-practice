4bf091f7533731687bd49614041b8d00b1a39f01
Bocong Sui <bocong@hotmail.com>
2023-02-08 23:31:37 +0800

    add solution 692

---
+++ src/Solution_692.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution_692 {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        for(String word: words) {
+            wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+        }
+
+        PriorityQueue<String> queue = new PriorityQueue<>((w1, w2) -> wordCountMap.get(w1) == wordCountMap.get(w2)? w2.compareTo(w1) : wordCountMap.get(w1) - wordCountMap.get(w2));
+
+        for(String word: wordCountMap.keySet()) {
+            queue.offer(word);
+            if(queue.size() > k) {
+                queue.poll();
+            }
+        }
+
+        List<String> answers = new ArrayList<>();
+        while(queue.size() > 0) {
+            answers.add(queue.poll());
+        }
+        Collections.reverse(answers);
+        return answers;
+    }
+}
