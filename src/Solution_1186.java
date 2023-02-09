1c305a9bffc5403f2af3667be40f67f22055f8d2
Bocong Sui <bocong@hotmail.com>
2023-02-10 07:49:06 +0800

    add solution 1186

---
+++ src/Solution_1186.java
@@ -0,0 +1,17 @@
+public class Solution_1186 {
+    public int maximumSum(int[] arr) {
+        int currentContSum = arr[0];
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(currentContSum, currentSum + arr[i]);
+            currentSum = Math.max(currentSum, arr[i]);
+            currentContSum = Math.max(currentContSum + arr[i], arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+}
