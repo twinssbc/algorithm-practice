f78e56bf52c8f446129ecfcb90ba2523345e4786
Bocong Sui <bocong@hotmail.com>
2023-02-27 23:06:37 +0800

    add InviteFriends

---
+++ design_practice/InviteFriends.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class InviteFriends {
+    public int inviteMostFriends(List<int[]> friendList, int num) {
+        HashMap<Integer, List<Integer>> friendConnections = new HashMap<>();
+        for(int[] friendPair: friendList) {
+            if(!friendConnections.containsKey(friendPair[0])) {
+                friendConnections.put(friendPair[0], new ArrayList<>());
+            }
+            friendConnections.get(friendPair[0]).add(friendPair[1]);
+            if(!friendConnections.containsKey(friendPair[1])) {
+                friendConnections.put(friendPair[1], new ArrayList<>());
+            }
+            friendConnections.get(friendPair[1]).add(friendPair[0]);
+        }
+
+        Set<Integer> remainingFriends = new HashSet<>();
+        for(int i = 1; i <= num; i++) {
+            remainingFriends.add(i);
+        }
+
+        return helper(friendConnections, remainingFriends);
+    }
+
+    public int helper(HashMap<Integer, List<Integer>> friendConnections, Set<Integer> remainingFriends) {
+        if(remainingFriends.size() <= 1) {
+            return remainingFriends.size();
+        }
+        int maxCount = 0;
+        for(Integer user: remainingFriends) {
+            Set<Integer> copiedSets = new HashSet<>(remainingFriends);
+            copiedSets.remove(user);
+            if(friendConnections.containsKey(user)) {
+                for (Integer friend : friendConnections.get(user)) {
+                    copiedSets.remove(friend);
+                }
+            }
+            maxCount = Math.max(maxCount, helper(friendConnections, copiedSets) + 1);
+        }
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        InviteFriends solution = new InviteFriends();
+
+        List<int[]> friends = new ArrayList<>();
+        friends.add(new int[] {1,2});
+        friends.add(new int[] {1,3});
+        friends.add(new int[] {4,5});
+        friends.add(new int[] {3,5});
+
+        int answer = solution.inviteMostFriends(friends,5);
+        assert answer==3;
+
+        friends.clear();
+        friends.add(new int[] {1,3});
+        friends.add(new int[] {1,4});
+        friends.add(new int[] {1,2});
+        answer = solution.inviteMostFriends(friends,4);
+        assert answer==3;
+
+        friends.clear();
+        friends.add(new int[] {1, 2});
+        friends.add(new int[] {1, 5});
+        friends.add(new int[] {2, 3});
+        friends.add(new int[] {2, 4});
+        friends.add(new int[] {3, 5});
+        friends.add(new int[] {3, 8});
+        friends.add(new int[] {4, 7});
+        friends.add(new int[] {4, 5});
+        friends.add(new int[] {5, 6});
+        friends.add(new int[] {5, 7});
+        friends.add(new int[] {5, 8});
+        friends.add(new int[] {6, 9});
+        friends.add(new int[] {7, 9});
+        friends.add(new int[] {8, 9});
+        friends.add(new int[] {8, 10});
+        friends.add(new int[] {9, 10});
+        answer = solution.inviteMostFriends(friends, 10);
+        assert answer == 5;
+    }
+}
