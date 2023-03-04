c1646ca1b3ab34a26add34221406c18e9f1f4511
Bocong Sui <bocong@hotmail.com>
2023-03-04 17:26:17 +0800

    add matching parenthesis

---
+++ design_practice/Parenthesis.java
@@ -0,0 +1,71 @@
+public class Parenthesis {
+    public int process(String input) {
+        int balance = 0;
+        for(char c: input.toCharArray()) {
+            if(c == '(') {
+                balance ++;
+            } else if(c == ')') {
+                balance--;
+            }
+        }
+
+        if(balance == 0 || Math.abs(balance) > 1) {
+            return 0;
+        }
+
+        if(balance == 1) {
+            StringBuilder sb = new StringBuilder();
+            int i = input.length() - 1;
+            while(i >= 0) {
+                if(input.charAt(i) == '(') {
+                    sb.append(')');
+                } else {
+                    sb.append('(');
+                }
+                i--;
+            }
+            input = sb.toString();
+        }
+
+        int count = 1;
+        balance = 0;
+        for(char c: input.toCharArray()) {
+            if(c == '(') {
+                balance++;
+            } else {
+                balance--;
+            }
+            if(balance == -1) {
+                break;
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Parenthesis solution = new Parenthesis();
+        int answer;
+        answer = solution.process("()())");
+        assert answer == 5;
+        answer = solution.process("()()()");
+        assert answer == 0;
+        answer = solution.process("");
+        assert answer == 0;
+        answer = solution.process(")()()");
+        assert answer == 1;
+        answer = solution.process("()()(");
+        assert answer == 1;
+        answer = solution.process("((())");
+        assert answer == 5;
+        answer = solution.process("(((())");
+        assert answer == 0;
+        answer = solution.process(")(())(");
+        assert answer == 0;
+        answer = solution.process(")())");
+        assert answer == 0;
+        answer = solution.process("()())()");
+        assert answer == 5;
+    }
+}
